8d13b53970d0123f28ce3cc2261d8f7017401980
libra <23025996+smirror@users.noreply.github.com>
2022-08-10 22:25:22 +0900

    add: add & sub

--- src/ecc.rs
+++ src/ecc.rs
@@ -1,23 +1,31 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct FieldElement {
-    num: u32,
-    prime: u32
+    num: isize,
+    prime:  isize,
 }
 
 impl FieldElement{
-    pub fn init(num: u32, prime: u32) -> FieldElement {
-        if num >= prime {
-            panic!("num {} not in field range 0 to {}", num, prime - 1);
-        }
+    pub fn init(num: isize, prime: isize) -> FieldElement {
+        // if num >= prime {
+        //     panic!("num {} not in field range 0 to {}", num, prime - 1);
+        // }
         FieldElement {
             num: num % prime,
             prime
         }
     }
+
+    pub fn add(&self, other: &FieldElement) -> FieldElement {
+        FieldElement::init(self.num + other.num, self.prime)
+    }
+
+    pub fn sub(&self, other: &FieldElement) -> FieldElement {
+        FieldElement::init(self.num - other.num, self.prime)
+    }
+
 }
 
 #[cfg(test)]
-
 mod tests{
     use crate::ecc::FieldElement;
 
@@ -25,8 +33,29 @@ mod tests{
     fn test_partial_eq(){
         let a = FieldElement::init(7, 13);
         let b = FieldElement::init(6, 13);
-        assert_eq!(a, a);
         assert_eq!(a == b, false);
+    }
+
+    #[test]
+    fn test_eq(){
+        let a = FieldElement::init(7, 13);
+        let b = FieldElement::init(6, 13);
         assert_eq!(a == a, true);
+    }
+
+    #[test]
+    fn test_add(){
+        let a = FieldElement::init(7, 13);
+        let b = FieldElement::init(6, 13);
+        let c = FieldElement::init(13, 13);
+        assert_eq!(a.add(&b), c);
+    }
+
+    #[test]
+    fn test_sub(){
+        let a = FieldElement::init(6, 13);
+        let b = FieldElement::init(7, 13);
+        let c = FieldElement::init(-1, 13);
+        assert_eq!(a.sub(&b), c);
     }
 }
