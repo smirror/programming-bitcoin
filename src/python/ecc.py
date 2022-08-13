0bdba5bea20474980511735eb3b56e4c76122d82
smirror <23025996+smirror@users.noreply.github.com>
2022-08-13 18:38:59 +0900

    Chapter03: Elliptic Curve Cryptography (#3)
    * add ex3_1
    
    * add ex3_2
    
    * Automated Python formatter changes
    
    * add scalar multiply
    
    * Automated Python formatter changes
    
    * fix README
    
    * add ex3_6
    
    * Automated Python formatter changes
    
    * add ex3_7
    
    * Automated Python formatter changes
    
    * add ex3_7 verify
    
    * Automated Python formatter changes

--- src/python/ecc.py
+++ src/python/ecc.py
@@ -1,3 +1,9 @@
+import hashlib
+import hmac
+
+from rsa.randnum import randint
+
+
 class FieldElement(object):
     def __init__(self, num, prime):
         # if num < 0 or num >= prime:
@@ -15,22 +21,35 @@ class FieldElement(object):
         return not (self.num == other.num and self.prime == other.prime)
 
     def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Cannot add two numbers in different Fields")
         return self.__class__((self.num + other.num) % self.prime, self.prime)
 
     def __sub__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Cannot subtract two numbers in different Fields")
         return self.__class__((self.num - other.num) % self.prime, self.prime)
 
     def __mul__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Cannot multiply two numbers in different Fields")
         return self.__class__((self.num * other.num) % self.prime, self.prime)
 
     def __pow__(self, n):
         return self.__class__(pow(self.num, n, self.prime), self.prime)
 
     def __truediv__(self, other):
-        return self.__mul__(other.__pow__(self.prime - 2))
+        if self.prime != other.prime:
+            raise TypeError("Cannot divide two numbers in different Fields")
+        num = (self.num * pow(other.num, self.prime - 2, self.prime)) % self.prime
+        return self.__class__(num, self.prime)
 
     def __floordiv__(self, other):
         return self.__truediv__(other)
+
+    def __rmul__(self, coefficient):
+        num = (self.num * coefficient) % self.prime
+        return self.__class__(num, self.prime)
 
 
 class Point:
@@ -80,10 +99,110 @@ class Point:
             y = s * (self.x - x) - self.y
             return self.__class__(x, y, self.a, self.b)
 
-        if self.x == other.x and self.y == other.y:
-            if self.y == 0 * self.x:
-                return self.__class__(None, None, self.a, self.b)
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+
+        if self == other:
             s = (3 * (self.x**2) + self.a) / (2 * self.y)
             x = s**2 - 2 * self.x
             y = s * (self.x - x) - self.y
             return self.__class__(x, y, self.a, self.b)
+
+    def __rmul__(self, coef):
+        current = self
+        result = self.__class__(None, None, self.a, self.b)
+        while coef:
+            if coef & 1:
+                result += current
+            current += current
+            coef >>= 1
+        return result
+
+
+P = 2**256 - 2**32 - 977
+
+
+class S256Field(FieldElement):
+    def __init__(self, num, prime=None):
+        super().__init__(num, P)
+
+    def __repr__(self):
+        return "{:x}".format(self.num).zfill(64)
+
+
+A = 0
+B = 7
+N = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+
+
+class S256Point(Point):
+    def __init__(self, x, y, a=None, b=None):
+        a, b = S256Field(A), S256Field(B)
+        if type(x) == int:
+            super().__init__(S256Field(x), S256Field(y), a, b)
+        else:
+            super().__init__(x, y, a, b)
+
+    def __rmul__(self, coef):
+        coef %= N
+        return super().__rmul__(coef)
+
+    def verify(self, z, sig):
+        s_inv = pow(sig.s, N - 2, N)
+        u = z * s_inv % N
+        v = sig.r * s_inv % N
+        total = u * G + v * self
+        return total.x.num == sig.r
+
+
+G = S256Point(
+    0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
+    0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8,
+)
+
+
+class Signature:
+    def __init__(self, r, s):
+        self.r = r
+        self.s = s
+
+    def __repr__(self):
+        return "Signature({:x}, {:x})".format(self.r, self.s)
+
+
+class PrivateKey:
+    def __init__(self, secret):
+        self.secret = secret
+        self.point = secret * G
+
+    def hex(self):
+        return "{:x}".format(self.secret).zfill(64)
+
+    def sign(self, z):
+        k = randint(N - 1)
+        r = (k * G).x.num
+        k_inv = pow(k, N - 2, N)
+        s = (z + r * self.secret) * k_inv % N
+        if s > N / 2:
+            s = N - s
+        return Signature(r, s)
+
+    def deterministic_generate(self, z):
+        k = b"\x00" * 32
+        v = b"\x01" * 32
+        if z > N:
+            z -= N
+        z_bytes = z.to_bytes(32, "big")
+        secret_bytes = self.secret.to_bytes(32, "big")
+        s256 = hashlib.sha256
+        k = hmac.new(k, v + b"\x00" + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        k = hmac.new(k, v + b"\x01" + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        while True:
+            v = hmac.new(k, v, s256).digest()
+            candidate = int.from_bytes(v, "big")
+            if candidate >= 1 and candidate < N:
+                return candidate  # <2>
+            k = hmac.new(k, v + b"\x00", s256).digest()
+            v = hmac.new(k, v, s256).digest()
--- src/python/ecc_test.py
+++ src/python/ecc_test.py
@@ -1,8 +1,9 @@
 import unittest
 from ecc import *
+from helper import hash256
 
 
-class MyTestCase(unittest.TestCase):
+class ECCTest(unittest.TestCase):
     def test_eq(self):
         a = FieldElement(44, 57)
         self.assertEqual(a == a, True)  # add assertion here
@@ -101,6 +102,92 @@ class MyTestCase(unittest.TestCase):
     def test_ex2_6(self):
         a = Point(-1, -1, 5, 7)
         print((a + a).x, (a + a).y)
+
+    def test_ex3_1(self):
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+        valid_points = [(192, 105), (17, 56), (1, 193)]
+        invalid_points = [(200, 119), (42, 99)]
+        for x_raw, y_raw in valid_points:
+            x = FieldElement(x_raw, prime)
+            y = FieldElement(y_raw, prime)
+            Point(x, y, a, b)
+        for x_raw, y_raw in invalid_points:
+            x = FieldElement(x_raw, prime)
+            y = FieldElement(y_raw, prime)
+            with self.assertRaises(ValueError):
+                Point(x, y, a, b)
+
+    def test_ex3_2(self):
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+        points = [[(170, 142), (60, 139)], [(47, 71), (17, 56)], [(143, 98), (76, 66)]]
+        for x_raw, y_raw in points:
+            x1 = FieldElement(x_raw[0], prime)
+            y1 = FieldElement(x_raw[1], prime)
+            x2 = FieldElement(y_raw[0], prime)
+            y2 = FieldElement(y_raw[1], prime)
+            c = Point(x1, y1, a, b)
+            d = Point(x2, y2, a, b)
+            ans = c + d
+            print(ans.x, ans.y)
+
+    def test_ex3_4(self):
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+        points = [
+            (2, 192, 105),
+            (2, 143, 98),
+            (2, 47, 71),
+            (4, 47, 71),
+            (8, 47, 71),
+            (21, 47, 71),
+        ]
+        for c, x_raw, y_raw in points:
+            x = FieldElement(x_raw, prime)
+            y = FieldElement(y_raw, prime)
+            d = Point(x, y, a, b)
+            ans = c * d
+            print(ans.x, ans.y)
+
+    def test_ex3_5(self):
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+        x = FieldElement(15, prime)
+        y = FieldElement(86, prime)
+        p = Point(x, y, a, b)
+        print(7 * p)
+
+    def test_ex3_6_1(self):
+        p = S256Point(
+            0x887387E452B8EACC4ACFDE10D9AAF7F6D9A0F975AABB10D006E4DA568744D06C,
+            0x61DE6D95231CD89026E286DF3B6AE4A894A3378E393E93A0F45B666329A0AE34,
+        )
+        z = 0xEC208BAA0FC1C19F708A9CA96FDEFF3AC3F230BB4A7BA4AEDE4942AD003C0F60
+        r = 0xAC8D1C87E51D0D441BE8B3DD5B05C8795B48875DFFE00B7FFCFAC23010D3A395
+        s = 0x68342CEFF8935EDEDD102DD876FFD6BA72D6A427A3EDB13D26EB0781CB423C4
+        self.assertTrue(p.verify(z, Signature(r, s)))
+
+    def test_ex3_6_2(self):
+        p = S256Point(
+            0x887387E452B8EACC4ACFDE10D9AAF7F6D9A0F975AABB10D006E4DA568744D06C,
+            0x61DE6D95231CD89026E286DF3B6AE4A894A3378E393E93A0F45B666329A0AE34,
+        )
+        z = 0x7C076FF316692A3D7EB3C3BB0F8B1488CF72E1AFCD929E29307032997A838A3D
+        r = 0xEFF69EF2B1BD93A66ED5219ADD4FB51E11A840F404876325A1E8FFE0529A2C
+        s = 0xC7207FEE197D27C618AEA621406F6BF5EF6FCA38681D82B2F06FDDBDCE6FEAB6
+        self.assertTrue(p.verify(z, Signature(r, s)))
+
+    def test_ex3_7(self):
+        e = 12345
+        z = int.from_bytes(hash256(b"Programming Bitcoin!"), "big")
+        pk = PrivateKey(e).sign(z)
+        print(pk)
+        print((e * G).verify(z, pk))
 
 
 if __name__ == "__main__":
