6ccf92874bd22d21ec58a1550daaca7e73445625
libra <23025996+smirror@users.noreply.github.com>
2022-09-03 23:01:35 +0900

    add chap5.1

---
+++ src/python/tx.py
@@ -0,0 +1,41 @@
+from helper import hash256
+
+
+class Tx:
+    def __init__(self, version, tx_ins, tx_outs, locktime, testnet=False):
+        self.version = version
+        self.tx_ins = tx_ins
+        self.tx_outs = tx_outs
+        self.locktime = locktime
+        self.testnet = testnet
+
+    def __repr__(self):
+        tx_ins = ''
+        for tx_in in self.tx_ins:
+            tx_ins += tx_in.__repr__() + '\n'
+        tx_outs = ''
+        for tx_out in tx_outs:
+            tx_outs + tx_out.__repr__() + '\n'
+        return 'Tx: {}\nversion: {}\nTx_ins:\n{}\nTx_outs:\n{}\nLocktime: {}'.format(
+            self.id(),
+            self.version,
+            tx_ins,
+            tx_outs,
+            self.locktime
+        )
+
+    def id(self):
+        """
+        人が読める16進数表記のトランザクションハッシュ
+        """
+        return self.hash().hex()
+
+    def hash(self):
+        """
+        トランザクションのバイナリハッシュ
+        """
+        return hash256(self.serialize())[::-1]
+
+    @classmethod
+    def parse(cls, serialization):
+        version = serialization.read(4)
