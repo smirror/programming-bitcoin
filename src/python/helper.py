1248ef004c206386cc56ae45a8e4a1131e28bae6
smirror <23025996+smirror@users.noreply.github.com>
2022-09-03 13:43:47 +0000

    Automated Python formatter changes

--- src/python/helper.py
+++ src/python/helper.py
@@ -60,19 +60,18 @@ def int_to_little_endian(n, length):
 
 
 class HelperTest(TestCase):
-
     def test_little_endian_to_int(self):
-        h = bytes.fromhex('99c3980000000000')
+        h = bytes.fromhex("99c3980000000000")
         want = 10011545
         self.assertEqual(little_endian_to_int(h), want)
-        h = bytes.fromhex('a135ef0100000000')
+        h = bytes.fromhex("a135ef0100000000")
         want = 32454049
         self.assertEqual(little_endian_to_int(h), want)
 
     def test_int_to_little_endian(self):
         n = 1
-        want = b'\x01\x00\x00\x00'
+        want = b"\x01\x00\x00\x00"
         self.assertEqual(int_to_little_endian(n, 4), want)
         n = 10011545
-        want = b'\x99\xc3\x98\x00\x00\x00\x00\x00'
+        want = b"\x99\xc3\x98\x00\x00\x00\x00\x00"
         self.assertEqual(int_to_little_endian(n, 8), want)
