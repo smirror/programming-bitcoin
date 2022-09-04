f74c53fb9c501c7dfa6659fc6c9d1beb6f8a60c3
smirror <23025996+smirror@users.noreply.github.com>
2022-09-04 12:46:40 +0000

    Automated Python formatter changes

--- src/python/tx.py
+++ src/python/tx.py
@@ -169,19 +169,19 @@ class TxFetcher:
     @classmethod
     def get_url(cls, testnet=False):
         if testnet:
-            return 'https://blockstream.info/testnet/api/'
+            return "https://blockstream.info/testnet/api/"
         else:
-            return 'https://blockstream.info/api/'
+            return "https://blockstream.info/api/"
 
     @classmethod
     def fetch(cls, tx_id, testnet=False, fresh=False):
         if fresh or (tx_id not in cls.cache):
-            url = '{}/tx/{}/hex'.format(cls.get_url(testnet), tx_id)
+            url = "{}/tx/{}/hex".format(cls.get_url(testnet), tx_id)
             response = requests.get(url)
             try:
                 raw = bytes.fromhex(response.text.strip())
             except ValueError:
-                raise ValueError('unexpected response: {}'.format(response.text))
+                raise ValueError("unexpected response: {}".format(response.text))
             if raw[4] == 0:
                 raw = raw[:4] + raw[6:]
                 tx = Tx.parse(BytesIO(raw), testnet=testnet)
@@ -189,15 +189,14 @@ class TxFetcher:
             else:
                 tx = Tx.parse(BytesIO(raw), testnet=testnet)
             if tx.id() != tx_id:
-                raise ValueError('not the same id: {} vs {}'.format(tx.id(),
-                                                                    tx_id))
+                raise ValueError("not the same id: {} vs {}".format(tx.id(), tx_id))
             cls.cache[tx_id] = tx
         cls.cache[tx_id].testnet = testnet
         return cls.cache[tx_id]
 
     @classmethod
     def load_cache(cls, filename):
-        disk_cache = json.loads(open(filename, 'r').read())
+        disk_cache = json.loads(open(filename, "r").read())
         for k, raw_hex in disk_cache.items():
             raw = bytes.fromhex(raw_hex)
             if raw[4] == 0:
@@ -210,7 +209,7 @@ class TxFetcher:
 
     @classmethod
     def dump_cache(cls, filename):
-        with open(filename, 'w') as f:
+        with open(filename, "w") as f:
             to_dump = {k: tx.serialize().hex() for k, tx in cls.cache.items()}
             s = json.dumps(to_dump, sort_keys=True, indent=4)
             f.write(s)
