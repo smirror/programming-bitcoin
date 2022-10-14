bbfbddf158b37857beba089ee5ed02c07eeaa68c
libra <23025996+smirror@users.noreply.github.com>
2022-10-14 22:48:57 +0900

    add ex6.2

--- src/python/op.py
+++ src/python/op.py
@@ -641,8 +641,20 @@ def op_hash256(stack):
 
 
 def op_checksig(stack, z):
-    raise NotImplementedError
-
+    if len(stack) < 2:
+        return False
+    sec_pubkey = stack.pop()
+    der_signature = stack.pop()[:-1]
+    try:
+        point = S256Point.parse(sec_pubkey)
+        sig = Signature.parse(der_signature)
+    except (ValueError, SyntaxError) as e:
+        return False
+    if point.verify(z, sig):
+        stack.append(encode_num(1))
+    else:
+        stack.append(encode_num(0))
+    return True
 
 def op_checksigverify(stack, z):
     return op_checksig(stack, z) and op_verify(stack)
