7138a56e7bd109098a99b555124845cea01c5c43
libra <23025996+smirror@users.noreply.github.com>
2023-02-24 19:49:35 +0900

    add main.rs

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+mod ecc;
+mod lib;
+
+fn main() {
+    let a = ecc::FieldElement::init(7, 13);
+    let b = ecc::FieldElement::init(6, 13);
+    let c = ecc::FieldElement::init(13, 13);
+    let d = ecc::FieldElement::init(-1, 13);
+    println!("{:?}, {:?}", a, b);
+    println!("{}", a == b);
+    println!("{}", a == a);
+    println!("{}", a.add(&b) == c);
+    println!("{}", b.sub(&a) == d);
+}
